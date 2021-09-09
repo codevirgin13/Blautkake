bc0f8ce3c373c18b233509807cd19c65f6c7e496
Ingrid Bjørndal <inghbjorndal@gmail.com>
2021-09-09 13:27:51 +0200

    commit

--- Blautkake.js
+++ Blautkake.js
@@ -1,2 +1,9 @@
+
 let egg=5/16*5;
 console.log("egg"+egg);
+let sukker=150/16*150;
+console.log("sukker"+sukker);
+let hvetemel=150/16*150;
+console.log("hvetemel"+sukker);
+let bakepulver=1/16*1;
+console.log("bakepulver"+bakepulver);
